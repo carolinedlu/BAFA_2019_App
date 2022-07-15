4e2ebeca865cb23a1f9a71ae3aa645100656a6d3
Jose Alexander Vindel Garduno <60977557+j-alex-vindel@users.noreply.github.com>
2022-07-15 11:13:10 +0100

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -15,13 +15,19 @@ st.sidebar.success("Select a page to visit.")
 
 st.markdown(
     """
-    Here I should write about the pages
+    ### The pages within this App tell the story about how I used some libraries in Python :snake: to aquire data, analyze it and implement a model on it.  
     **👈 Select a Page for you to explore!
     ### Sections to explore in this App  
     - About BAFA :football:
     - Data Collection and Questions :computer:
     - Findings :mag_right: :bar_chart:
     - Models :chart_with_upwards_trend:
+
+    ### The overall approach was
+    - Webscrapping and API's
+    - Data Integration 
+    - Data Analysis
+    - ML
 """
 )
 
@@ -29,10 +35,21 @@ st.markdown("## About me")
 
 st.markdown(
     """
-    ### Some details about me
-    - Background
-    - Github
-    - LinkedIn
+    #### PhD Candidate on management science at Strathclyde University Business School. Currently working on bi-level optimization. 
+    ##### Interests:
+    - Linear Optimization
+    - Mixed Integer Linear Optimization
+    - Discrete Network Optimization
+    - Data Analysis  
 
 """
 )
+
+st.markdown(
+    """ 
+    ### Contact Details
+    - e-mail : alexander_vindel@gmail.com
+    - LinkedIn : [alexander-vindel](https://www.linkedin.com/in/alexander-vindel/)
+    - GitHub : [@j-alex-vindel](https://github.com/j-alex-vindel)
+
+    """)
